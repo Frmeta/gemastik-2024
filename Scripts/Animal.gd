0febc8e48a819ac0f79b031cac9bb780a468b2fc
Fredo Melvern Tanzil <55704134+Frmeta@users.noreply.github.com>
2024-06-07 22:05:33 +0700

    hasil scan muncul di almanac

--- Scripts/Animal.gd
+++ Scripts/Animal.gd
@@ -7,8 +7,10 @@ var scan_progress = 0.0 # range dari 0 sampai 1
 
 
 func _process(delta):
-	var a = $Kodomo_Skeleton/Skeleton3D/Komodo_body_001.get_active_material(0)
+	var a = mesh.get_active_material(0)
 	a.next_pass.set_shader_parameter("Dissolve_Height", scan_progress)
 	
 	if scan_progress >= 1:
 		print(name + " has been scanned")
+		if !GM.scanned_animal.has(name):
+			GM.scanned_animal.append(name)
--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -4,6 +4,10 @@ var doni: CharacterBody3D
 
 var last_checkpoint_position:Vector3
 
+
 var data_file_number
 
-var explored_level
+var explored_level = 0
+var current_level
+
+var scanned_animal := []
