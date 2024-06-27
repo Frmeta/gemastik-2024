8c431664ebd056b0610c213cedc70d5554a537be
Fredo Melvern Tanzil <55704134+Frmeta@users.noreply.github.com>
2024-06-27 22:05:33 +0700

    fix dialog trap 2x

--- Scenes/Game/trap_dialog_trigger.gd
+++ Scenes/Game/trap_dialog_trigger.gd
@@ -2,8 +2,12 @@ extends Area3D
 
 @export var file_name:String
 
+var done = false
+
 func _on_body_entered(body):
-	GM.doni.stop_move()
-	EventDistributor.emit_signal("start_dialogue","res://dialogue/"+file_name+".json")
-	await EventDistributor.end_dialogue
-	GM.doni.allow_move()
+	if !done:
+		done = true
+		GM.doni.stop_move()
+		EventDistributor.emit_signal("start_dialogue","res://dialogue/"+file_name+".json")
+		await EventDistributor.end_dialogue
+		GM.doni.allow_move()
