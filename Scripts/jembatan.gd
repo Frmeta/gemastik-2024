7b6bd89bfc9fb64664556ad64d3831d544989647
KenKomKom <kenichikomala@gmail.com>
2024-08-12 20:29:38 +0700

    bug fix a bunch of jembatan bug

--- Scripts/jembatan.gd
+++ Scripts/jembatan.gd
@@ -5,21 +5,19 @@ extends StaticBody3D
 @export_range(1, 3) var tipe_jembatan=1
 @export var is_pangkal:=false
 
-var _start_position : Vector3
-
 func _ready():
+	get_node("base1").visible=false
 	if tipe_pager!=0:
 		get_node("belakang"+str(tipe_pager)).visible=true
 		get_node("depan"+str(tipe_pager)).visible=true
 	if is_pangkal:
 		for i in range(1,4):
-			get_node("base"+str(tipe_pager)).visible = false
+			get_node("base"+str(i)).visible = false
 		get_node("pangkal"+str(tipe_jembatan)).visible=true
 	else:
 		for i in range(1,4):
-			get_node("pangkal"+str(tipe_pager)).visible = false
+			get_node("pangkal"+str(i)).visible = false
 		get_node("base"+str(tipe_jembatan)).visible=true
-	_start_position = global_position
 
 func _on_area_3d_body_entered(body):
 	if body is Player and can_fall:
