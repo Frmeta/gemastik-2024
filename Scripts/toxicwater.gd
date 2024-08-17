a282a851920d80ec51988baaf5168da9b3022f8d
KenKomKom <kenichikomala@gmail.com>
2024-08-18 01:13:41 +0700

    add cliff behind vine level 4, perangkap added

--- Scripts/toxicwater.gd
+++ Scripts/toxicwater.gd
@@ -7,8 +7,9 @@ func _ready():
 	water_shader.get_surface_override_material(0).set_shader_parameter("scale",self.scale.y)
 	water_shader.get_surface_override_material(0).set_shader_parameter("scale2",self.scale2)
 
-func _on_body_entered(body: Player):
-	body.stop_move()
-	await get_tree().create_timer(0.2).timeout
-	body.respawn()
-	body.allow_move()
+func _on_body_entered(body):
+	if body is Player:
+		body.stop_move()
+		await get_tree().create_timer(0.2).timeout
+		body.respawn()
+		body.allow_move()
