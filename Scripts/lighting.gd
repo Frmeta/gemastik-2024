8829b5d9d4846f561e728ace96f03d3981ff7a5f
KenKomKom <kenichikomala@gmail.com>
2024-09-04 21:06:52 +0700

    fix crash

--- Scripts/lighting.gd
+++ Scripts/lighting.gd
@@ -3,7 +3,7 @@ extends OmniLight3D
 var rng = RandomNumberGenerator.new()
 
 func _on_timer_timeout():
-	global_position.z = GM.doni.global_position.z+rng.randf_range(-50,50)
+	global_position.z = GM.doni.global_position.z+rng.randf_range(-30,30)
 	omni_range = 4096
 	await get_tree().create_timer(0.1).timeout
 	omni_range = 0
