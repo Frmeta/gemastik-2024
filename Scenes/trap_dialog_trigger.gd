8eeefd5411b5f9ccfa4be8430c65deb1675262c1
KenKomKom <kenichikomala@gmail.com>
2024-09-05 14:09:36 +0700

    babi kabur

--- Scenes/trap_dialog_trigger.gd
+++ Scenes/trap_dialog_trigger.gd
@@ -8,11 +8,11 @@ var done = false
 func _on_body_entered(body):
 	if !done:
 		done = true
-		if file_name!="dikejar_pemburu":
+		if file_name!="dikejar_pemburu" and file_name!="post_pemburu":
 			GM.doni.stop_move()
 			EventDistributor.emit_signal("start_dialogue","res://dialogue/"+file_name+".json")
 		else:
-			EventDistributor.emit_signal("start_dialogue_not_stop","res://dialogue/"+file_name+".json")
+			EventDistributor.emit_signal("start_dialogue_not_stop_timed","res://dialogue/"+file_name+".json")
 		if move_cam_to!=null:
 			EventDistributor.emit_signal("change_target", move_cam_to)
 		await EventDistributor.end_dialogue
